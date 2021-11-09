781beea40b1c8fad419107a7b8a267c8621dab46
sven-frei <sven.frei@axa.ch>
2021-11-09 15:21:28 +0100

    test console output c-sharp

--- sh/c-sharp-test.sh
+++ sh/c-sharp-test.sh
@@ -3,5 +3,5 @@
 UUID=$(curl -X POST -H "Content-Type: application/json" -d "{\"width\":1000,\"height\":1000}" http://localhost:8080/mandelbrot)
 
 echo "\n"
-curl -X GET "http://localhost:8080/mandelbrot/?fileUuid=$UUID" > log.txt
+curl -X GET "http://localhost:8080/mandelbrot/?fileUuid=$UUID"
 echo "\n"
