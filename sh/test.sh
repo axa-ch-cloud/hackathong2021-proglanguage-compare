4fa6bc15ee2871497349df9ac9e9d77b06d933be
Luca Mele <luca.mele@axa-winterthur.ch>
2021-11-09 13:44:28 +0100

    write to log file

--- sh/test.sh
+++ sh/test.sh
@@ -3,5 +3,5 @@
 UUID=$(curl -X POST -H "Content-Type: application/json" -d "{\"width\":\"10\",\"height\":\"10\"}" http://localhost:8080/mandelbrot)
 
 echo "\n"
-curl -X GET "http://localhost:8080/mandelbrot/$UUID"
+curl -X GET "http://localhost:8080/mandelbrot/$UUID" > log.txt
 echo "\n"
