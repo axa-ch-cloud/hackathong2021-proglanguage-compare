b242b214d6e00085f075e00555455638b8c98eaf
Luca Mele <luca.mele@axa-winterthur.ch>
2021-11-09 11:41:21 +0100

    Basic post

--- node/index.js
+++ node/index.js
@@ -1,12 +1,37 @@
+#!/usr/bin/env node
+
 const express = require('express');
-const cors = require('cors')
+const { mandelbrot } = require('./mandelbrot');
 const app = express();
 const port = 3000;
 
-app.use(cors());
+app.use(express.json());
+
+app.post('/mandelbrot', (req, res) => {
+
+  console.log(req.body.seed);
+  res.send( mandelbrot(req.body.seed));
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(`
+
+    <script>
+    (async () => {
+      const rawResponse = await fetch('http://localhost:3000/mandelbrot', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({seed: 4000})
+      });
+      const content = await rawResponse.json();
+
+      console.log(content);
+    })();
+    </script>
+  `)
 });
 
 app.listen(port, () => {
---
+++ node/mandelbrot/index.js
@@ -0,0 +1,19 @@
+const MAX_ITERATION = 80
+function mandelbrot(c) {
+    let z = { x: 0, y: 0 }, n = 0, p, d;
+    do {
+        p = {
+            x: Math.pow(z.x, 2) - Math.pow(z.y, 2),
+            y: 2 * z.x * z.y
+        }
+        z = {
+            x: p.x + c.x,
+            y: p.y + c.y
+        }
+        d = Math.sqrt(Math.pow(z.x, 2) + Math.pow(z.y, 2))
+        n += 1
+    } while (d <= 2 && n < MAX_ITERATION)
+    return [n, d <= 2]
+}
+
+module.exports.mandelbrot = mandelbrot;
