e0449353e063388fa1bf714ca69ffc907a8bf7c0
Matthias Gubler <matthiasgubler@me.com>
2021-11-10 11:32:45 +0100

    Increase testcase

--- sh/c-sharp-test.sh
+++ sh/c-sharp-test.sh
@@ -1,6 +1,6 @@
 #!/bin/(shell)
 
-UUID=$(curl --silent  -X POST -H "Content-Type: application/json" -d "{\"width\":1000,\"height\":1000}" http://localhost:8080/mandelbrot)
+UUID=$(curl --silent  -X POST -H "Content-Type: application/json" -d "{\"width\":10000,\"height\":10000}" http://localhost:8080/mandelbrot)
 
 echo "\n"
 curl --silent  -X GET "http://localhost:8080/mandelbrot/?fileUuid=$UUID" > log.txt
