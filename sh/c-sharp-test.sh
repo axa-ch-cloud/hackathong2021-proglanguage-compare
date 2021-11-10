900c03e73a39e1364d9c15e32ea06e23e707bcbd
sven-frei <sven.frei@axa.ch>
2021-11-10 10:09:39 +0100

    Last change c#.yml

--- sh/c-sharp-test.sh
+++ sh/c-sharp-test.sh
@@ -2,8 +2,6 @@
 
 UUID=$(curl --silent  -X POST -H "Content-Type: application/json" -d "{\"width\":1000,\"height\":1000}" http://localhost:8080/mandelbrot)
 
-echo "POST start"
-
 echo "\n"
 curl --silent  -X GET "http://localhost:8080/mandelbrot/?fileUuid=$UUID" > log.txt
 echo "\n"
