dc19d92dbe2803d818d801695747db50e893fac2
sven-frei <sven.frei@axa.ch>
2021-11-10 14:07:21 +0100

    Reverted dll start.

--- sh/c-sharp-test.sh
+++ sh/c-sharp-test.sh
@@ -1,7 +1,7 @@
 #!/bin/(shell)
 
-UUID=$(curl --silent  -X POST -H "Content-Type: application/json" -d "{\"width\":10000,\"height\":10000}" http://localhost:5000/mandelbrot)
+UUID=$(curl --silent  -X POST -H "Content-Type: application/json" -d "{\"width\":10000,\"height\":10000}" http://localhost:8080/mandelbrot)
 
 echo "\n"
-curl --silent  -X GET "http://localhost:5000/mandelbrot/?fileUuid=$UUID" > log.txt
+curl --silent  -X GET "http://localhost:8080/mandelbrot/?fileUuid=$UUID" > log.txt
 echo "\n"
